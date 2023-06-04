5d3f4ab174399bd498ec8749b4914e11e8e52a07
luissantanaescu <luis.santanaescu@alumnos.upm.es>
2023-06-04 17:27:51 +0200

    Pantalla de Login
    No se puede realizar el login aún, pero ya está creada la actividad y la pantalla.
    
    Se ha añadido en el manifest para que la app inicie con el Login

---
+++ terraCEOapp/app/src/main/java/com/example/terraceoapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.terraceoapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Código adicional para la actividad de inicio de sesión
+        // ...
+    }
+}
